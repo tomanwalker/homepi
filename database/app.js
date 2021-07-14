66aaea81795aa7bb28c6d8e02292fdc6aaa068a6
AlexShkunov <toman22@yandex.ru>
2021-07-14 10:57:26 +0300

    work on database

--- database/app.js
+++ database/app.js
@@ -1,20 +1,34 @@
 
 // ## dependencies
+var fs = require('fs');
 var Datastore = require('nedb');
 
 // ## config
+var folder = './data/';
+
 var neOpts = {
-	filename: './test.db',
+	filename: folder + 'test.db',
 	timestampData: true,
 	autoload: true
 };
 
-var db = new Datastore(neOpts);
+var testdb = new Datastore(neOpts);
 
 // ## func
+var listTables = function(){
+	return fs.readdirSync(folder);
+};
 
 // ## flow
 
-db.insert({test: 123});
+var tables = listTables();
+console.log( tables.map(x => x.replace('.db', '')) );
+
+testdb.insert({test: 123});
+
+testdb.find({ test: 123 }, function (err, docs) {
+	console.log( docs );
+});
+
 
 

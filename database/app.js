2a0f102f55f380c84d3c0ac0e7192989073e57a1
AlexShkunov <toman22@yandex.ru>
2021-07-19 10:17:44 +0300

    work on custom file DB - probably shouldnt

--- database/app.js
+++ database/app.js
@@ -1,34 +1,53 @@
 
 // ## dependencies
 var fs = require('fs');
-var Datastore = require('nedb');
 
 // ## config
 var folder = './data/';
-
-var neOpts = {
-	filename: folder + 'test.db',
-	timestampData: true,
-	autoload: true
-};
-
-var testdb = new Datastore(neOpts);
+var testTable = 'test';
 
 // ## func
 var listTables = function(){
-	return fs.readdirSync(folder);
+	var arr = fs.readdirSync(folder);
+	return arr.map(x => x.replace('.db', ''));
+};
+var listDocuments = function(tableName){
+	var arr = fs.readdirSync(folder + tableName);
+	return arr.map(x => x.replace('.txt', ''));
+};
+var insertDoc = function(tableName, doc){
+	var str = JSON.stringify(doc, null, 2);
+	var filePath = folder + tableName + '/' + doc._id + '.txt';
+	
+	return fs.writeFileSync(filePath, str);
+};
+var search = function(tableName, query){
+	return null;
+};
+var index = function(tableName){
+	// async
+	setTimeout(function(){
+		
+	}, 10);
 };
 
 // ## flow
 
 var tables = listTables();
-console.log( tables.map(x => x.replace('.db', '')) );
+console.log( 'tables = %j', tables );
 
-testdb.insert({test: 123});
+var docs = listDocuments(testTable);
+console.log( 'docs[%s] = %j', testTable, docs );
 
-testdb.find({ test: 123 }, function (err, docs) {
-	console.log( docs );
-});
+insertDoc(testTable, { _id: '123', here: 'abc' });
+index();
+
+setTimeout(function(){
+	
+	var result = search(testTable, {here: 'abc' });
+	console.log( 'result = %j', result );
+	
+}, 50);
 
 
 

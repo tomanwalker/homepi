957d86885da8eabba34c1e2b64231dc9474134bd
AlexShkunov <toman22@yandex.ru>
2021-10-19 10:00:18 +0300

    wokk on deploy script for Reminders

---
+++ services/reminders/deploy.sh
@@ -0,0 +1,18 @@
+
+SERVICE_NAME=node_reminder
+
+npm install
+
+#DIR=$(pwd)
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(realpath "${DIR}")"
+echo $DIR
+
+cp $DIR/service.sh /etc/init.d/$SERVICE_NAME
+sed -i "s/<DIR>/$DIR/" /etc/init.d/$SERVICE_NAME
+chmod +x /etc/init.d/$SERVICE_NAME
+
+#service $SERVICE_NAME start
+#update-rc.d $SERVICE_NAME defaults
+
+
